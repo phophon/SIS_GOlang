515987788fbc0f8bd009c1cd6d8d70ec996b72c4
phophon <phophon.i@cmkl.ac.th>
2020-08-26 19:08:29 +0700

    commit config server

--- server.go
+++ server.go
@@ -31,15 +31,48 @@ type JSONWebKeys struct {
    X5c []string
 }
 
-type Profile struct{
-	Name string
-	Uuid int
-}
+type Profile struct {
+	First_name  string
+	Last_name      string
+	Program string
+	Cmkl_email string
+	UUID int
+	Photo string
+	Contact struct {
+	   Phone_number string
+	   Personnal_email string
+	   Second_email string
+	}
+	Emergency [2]EmergencyContact
+	Useraddress [3]Address
+ }
+
+ type Address struct{
+	Addressstatus string
+	City string
+	State string
+	Zip string
+	Country string
+ }
+
+ type EmergencyContact struct {
+	Firstname string
+	Lastname string
+	Relationship string
+	Phone string
+	Email string
+ }
+
+// type Profile struct{
+// 	Name string
+// 	Uuid int
+// }
 
 
 var HomePage = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
     var data Profile
     var emergencyContact EmergencyContact
+    var address Address
     // data.Name = "Pinyarat"
     // data.Uuid = 109877189
 
@@ -52,25 +85,41 @@ var HomePage = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
     data.Contact.Phone_number = "0965436527"
     data.Contact.Personnal_email = "phophon.i@cmkl.ac.th"
     data.Contact.Second_email = "phophon.i@cmkl.ac.th"
-    data.Address.Addressstatus = "235/111 M.6 pruksa village"
-    data.Address.City = "samutprakarn"
-    data.Address.State = "bangmeaung"
-    data.Address.Zip = "10270"
-    data.Address.Country = "Thailand"
+
+    address.Addressstatus = "235/111 M.6 pruksa village"
+    address.City = "samutprakarn"
+    address.State = "bangmeaung"
+    address.Zip = "10270"
+    address.Country = "Thailand"
+    data.Useraddress[0] = address
+
+    address.Addressstatus = "235/111 M.6 pruksa village"
+    address.City = "samutprakarn"
+    address.State = "bangmeaung"
+    address.Zip = "10270"
+    address.Country = "Thailand"
+    data.Useraddress[1] = address
+
+    address.Addressstatus = "235/111 M.6 pruksa village"
+    address.City = "samutprakarn"
+    address.State = "bangmeaung"
+    address.Zip = "10270"
+    address.Country = "Thailand"
+    data.Useraddress[2] = address
 
     emergencyContact.Firstname = ""
     emergencyContact.Lastname = ""
     emergencyContact.Relationship = ""
     emergencyContact.Phone = ""
     emergencyContact.Email = ""
-    data.Emergency = append(data.Emergency, emergencyContact)
+    data.Emergency[0] = emergencyContact
 
     emergencyContact.Firstname = "montean"
     emergencyContact.Lastname = "puengkeaw"
     emergencyContact.Relationship = "Dad"
     emergencyContact.Phone = "0952262928"
     emergencyContact.Email = "montean226@hotmail.com"
-    data.Emergency = append(data.Emergency, emergencyContact)
+    data.Emergency[1] = emergencyContact
 
 
     // reqBody, err := json.Marshal(map[string]string{})
@@ -154,9 +203,9 @@ func StartServer() {
    r.HandleFunc("/api/v1/home", HomePage).Methods("POST")
    r.HandleFunc("/api/v1/callback", callback.CallbackHandler).Methods("GET")
    r.HandleFunc("/api/v1/login", login.LoginHandler).Methods("GET")
-   r.Handle("/api/v1/profile", jwtMiddleware.Handler(api.UpdateProfileHandler)).Methods("POST")
    r.Handle("/api/v1/profile", jwtMiddleware.Handler(api.ProfileApiHandler)).Methods("GET")
-   r.Handle("/api/v1/enroll", jwtMiddleware.Handler(api.EnrollmentApiHandler)).Methods("GET")
+   r.Handle("/api/v1/profile", api.UpdateProfileHandler).Methods("POST")
+   r.Handle("/api/v1/enroll", api.EnrollmentApiHandler).Methods("GET")
    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
    corsWrapper := cors.New(cors.Options{
